25ee5c187a8121133c6d774b48d47e1153de8ca4
Augie <augustdoebling@gmail.com>
2015-05-02 16:02:06 -0700

    Point Class Added

--- src/com/ooqle/game/Point.java
+++ src/com/ooqle/game/Point.java
@@ -5,5 +5,21 @@ package com.ooqle.game;
 
 public class Point
 {
+    private int x;
+    private int y;
 
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public boolean adjecent(Point p2)
+    {
+        return ((this.x == p2.x && Math.abs(this.y - p2.y) == 1) || (this.y == p2.y && Math.abs(this.x - p2.x) == 1));
+    }
+
+    public int distanceSq(Point p2)
+    {
+        return ((this.x - p2.x) * (this.x - p2.x)) + ((this.y - p2.y) * (this.y - p2.y));
+    }
 }
