c635064494cc579867a9c0ac372875e526349d32
Kenny Williams <kenny@kennyjwilliams.com>
2015-05-03 23:20:34 -0700

    Fully implemented toVein and nextPosition

--- src/com/ooqle/game/entity/OreBlob.java
+++ src/com/ooqle/game/entity/OreBlob.java
@@ -4,6 +4,9 @@ package com.ooqle.game.entity;
 */
 
 import com.ooqle.game.Point;
+import com.ooqle.game.WorldModel;
+import com.ooqle.game.util.GameUtils;
+import com.ooqle.game.util.Tuple;
 
 public class OreBlob extends MovableActor
 {
@@ -13,14 +16,45 @@ public class OreBlob extends MovableActor
         super(name, "unknown", position, rate);
     }
 
-    public void toVein()
+    public Tuple toVein(WorldModel world, Vein vein)
     {
-        //TODO: Implement me
+        Point entityPt = this.getPosition().clone();
+        if (vein == null)
+        {
+            return new Tuple(entityPt, false);
+        }
+        Point veinPt = vein.getPosition().clone();
+        if(entityPt.adjacent(veinPt))
+        {
+            vein.removeEntity(world);
+            return new Tuple(veinPt, true);
+        }else
+        {
+            Point newPt = this.nextPosition(world, veinPt);
+            WorldObject oldEntity = world.getTileOccpant(newPt);
+            if(oldEntity instanceof Ore)
+            {
+                ((Ore) oldEntity).removeEntity(world);
+            }
+            return new Tuple(world.moveEntity(this, newPt), false);
+        }
     }
 
-    public void nextPosition()
+    public Point nextPosition(WorldModel world, Point destPt)
     {
-        //TODO: Implement me
+        int horiz = GameUtils.sign(destPt.getX() - this.getPosition().getX());
+        Point newPt = new Point(this.getPosition().getX() + horiz, this.getPosition().getY());
+
+        if(horiz == 0 || (world.isOccupied(newPt) && !(world.getTileOccpant(newPt) instanceof Ore)))
+        {
+            int vert = GameUtils.sign(destPt.getY() - this.getPosition().getY());
+            newPt = new Point(this.getPosition().getX(), this.getPosition().getY() +  vert);
+            if(vert == 0 || !(world.getTileOccpant(newPt) instanceof Ore))
+            {
+                newPt = new Point(this.getPosition().getX(), this.getPosition().getY());
+            }
+        }
+        return newPt;
     }
 
     public String entityString()
