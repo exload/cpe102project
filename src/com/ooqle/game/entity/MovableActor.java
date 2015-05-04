17947b6da9716f2fda4941cd17c433a6305b5748
Kenny Williams <kenny@kennyjwilliams.com>
2015-05-03 22:47:30 -0700

    Began implementing nextPosition. (currently not working)

--- src/com/ooqle/game/entity/MovableActor.java
+++ src/com/ooqle/game/entity/MovableActor.java
@@ -5,11 +5,10 @@ package com.ooqle.game.entity;
 
 import com.ooqle.game.Point;
 import com.ooqle.game.WorldModel;
+import com.ooqle.game.util.GameUtils;
 
 public class MovableActor extends AnimatedActor
 {
-
-
     public MovableActor(String name, String type, Point position, int rate)
     {
         super(name, type, position, rate);
@@ -17,7 +16,13 @@ public class MovableActor extends AnimatedActor
 
     public Point nextPosition(WorldModel world, Point destPt)
     {
-        //TODO: Implement me
-        return null;
+        int horiz = GameUtils.sign(destPt.getX() - this.getPosition().getX());
+        Point newPt = new Point(this.getPosition().getX() + horiz, this.getPosition().getY());
+
+        if(horiz == 0 || world.isOccupied(newPt))
+        {
+            int vert = GameUtils.sign(destPt.getY() - this.getPosition().getY());
+
+        }
     }
 }
