97039c03bcdb343d8ea807d4092e5d09d5afcbb2
Augie <augustdoebling@gmail.com>
2015-05-04 21:41:30 -0700

    pushed testcases

--- src/test/java/org/ooqle/TestCases.java
+++ src/test/java/org/ooqle/TestCases.java
@@ -5,6 +5,7 @@ package test.java.org.ooqle;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import test.java.org.ooqle.entity.AnimatedActorTest;
 import test.java.org.ooqle.entity.EntityTests;
 
 @RunWith(Suite.class)
@@ -12,6 +13,7 @@ import test.java.org.ooqle.entity.EntityTests;
         PointTest.class,
         EntityTests.class,
         GridTest.class,
-        WorldModelTest.class
+        WorldModelTest.class,
+        AnimatedActorTest.class
 })
 public class TestCases{}
