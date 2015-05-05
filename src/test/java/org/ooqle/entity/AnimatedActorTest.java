8182d2e208c19db2d1edc1b9e86d4507a8553b30
Augie <augustdoebling@gmail.com>
2015-05-04 21:42:03 -0700

    AA test pushed

---
+++ src/test/java/org/ooqle/entity/AnimatedActorTest.java
@@ -0,0 +1,18 @@
+package test.java.org.ooqle.entity;
+
+import com.ooqle.game.Point;
+import com.ooqle.game.entity.AnimatedActor;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by augiedoebling on 5/4/15.
+ */
+public class AnimatedActorTest {
+    @Test
+    public void testGetAnimationRate() {
+        AnimatedActor actor = new AnimatedActor("augie", "actor", new Point(1, 1), 2, 4);
+        assertEquals(actor.getAnimationRate(), 4);
+    }
+}
