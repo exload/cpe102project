a6db17b917020a0e4d046e928d71711aa63edc3a
Kenny Williams <kenny@kennyjwilliams.com>
2015-05-04 21:59:08 -0700

    Added VeinTests

---
+++ src/test/java/org/ooqle/entity/VeinTests.java
@@ -0,0 +1,49 @@
+package test.java.org.ooqle.entity;
+/*
+* @author Kenny Williams
+*/
+
+import com.ooqle.game.Point;
+import com.ooqle.game.WorldModel;
+import com.ooqle.game.entity.Background;
+import com.ooqle.game.entity.Entity;
+import com.ooqle.game.entity.Vein;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class VeinTests
+{
+    private Vein v = new Vein("vein", new Point(0, 0), 1, 1);
+    private WorldModel world = new WorldModel(10, 10, new Background("bg"));
+
+    @Test
+    public void testResourceDistance()
+    {
+        assertEquals(1, v.getResourceDistance());
+    }
+
+    @Test
+    public void testFindOpenAround1()
+    {
+        Point p = v.findOpenAround(world);
+        assertEquals(new Point(0, 0), p);
+    }
+
+    @Test
+    public void testFindOpenAround2()
+    {
+        world.addEntity(new Entity("e", "t", new Point(0, 0), 1));
+        Point p = v.findOpenAround(world);
+        assertEquals(new Point(1, 0), p);
+    }
+
+    @Test
+    public void testEntityString()
+    {
+        String s = " ";
+        String expected = v.getType() + s + v.getName() + s + v.getPosition().toString()
+                + s + v.getRate() + s + v.getResourceDistance();
+        assertEquals(expected, v.entityString());
+    }
+}
