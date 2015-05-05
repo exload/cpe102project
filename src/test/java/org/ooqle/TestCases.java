96cf64dc34d91a4edde01aa4584057714344e913
Kenny Williams <kenny@kennyjwilliams.com>
2015-05-04 21:45:36 -0700

    Added TupleTests

--- src/test/java/org/ooqle/TestCases.java
+++ src/test/java/org/ooqle/TestCases.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import test.java.org.ooqle.entity.AnimatedActorTest;
 import test.java.org.ooqle.entity.EntityTests;
+import test.java.org.ooqle.util.TupleTests;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
@@ -14,6 +15,7 @@ import test.java.org.ooqle.entity.EntityTests;
         EntityTests.class,
         GridTest.class,
         WorldModelTest.class,
-        AnimatedActorTest.class
+        AnimatedActorTest.class,
+        TupleTests.class
 })
 public class TestCases{}
