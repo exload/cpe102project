cfd67c8312eccd4ed9c50eff0c392c8c3a18b055
Kenny Williams <kenny@kennyjwilliams.com>
2015-05-04 21:35:35 -0700

    Added test cases for Actor

--- src/test/java/org/ooqle/entity/ActorTests.java
+++ src/test/java/org/ooqle/entity/ActorTests.java
@@ -3,7 +3,26 @@ package test.java.org.ooqle.entity;
 * @author Kenny Williams
 */
 
+import com.ooqle.game.Point;
+import com.ooqle.game.entity.Actor;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ActorTests
 {
+    private Actor a = new Actor("name", "t", new Point(1, 1), 2);
 
+    @Test
+    public void testGetResourceCount()
+    {
+        assertEquals(0, a.getResourceCount());
+    }
+
+    @Test
+    public void testSetResourceCount()
+    {
+        a.setResourceCount(2);
+        assertEquals(2, a.getResourceCount());
+    }
 }
