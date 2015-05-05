e505a2557859590c48424046f2d3a91a5cc5bb06
Kenny Williams <kenny@kennyjwilliams.com>
2015-05-04 21:12:02 -0700

    Fixed issue with Obstacle not inheriting correctly

--- src/com/ooqle/game/entity/Obstacle.java
+++ src/com/ooqle/game/entity/Obstacle.java
@@ -5,10 +5,10 @@ package com.ooqle.game.entity;
 
 import com.ooqle.game.Point;
 
-public class Obstacle extends WorldObject
+public class Obstacle extends Entity
 {
     public Obstacle(String name, Point position)
     {
-        super(name, "obstacle", position);
+        super(name, "obstacle", position, 0);
     }
 }
