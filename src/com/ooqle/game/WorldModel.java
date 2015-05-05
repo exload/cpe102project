a85983390d403a8c51386fb87adebc7b66ef911a
Kenny Williams <kenny@kennyjwilliams.com>
2015-05-04 23:00:23 -0700

    Fixed bug in WorldModel where first was never set to false so the incorrect body of the if statment was always being ran

--- src/com/ooqle/game/WorldModel.java
+++ src/com/ooqle/game/WorldModel.java
@@ -177,6 +177,7 @@ public class WorldModel {
                 {
                     entity = other.getKey();
                     integer = other.getValue();
+                    first = false;
                 }
                 else
                 {
