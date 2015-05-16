fa97391af1bed3de446dde6bbc8414870178f4a6
Kenny Williams <kenny@kennyjwilliams.com>
2015-05-16 13:50:39 -0700

    Added createTransformAction and schedule

--- src/com/ooqle/game/entity/Ore.java
+++ src/com/ooqle/game/entity/Ore.java
@@ -3,9 +3,14 @@ package com.ooqle.game.entity;
 * @author Kenny Williams
 */
 
+import com.ooqle.game.ActionManager;
 import com.ooqle.game.Point;
+import com.ooqle.game.World;
+import com.ooqle.game.util.Action;
+import com.ooqle.game.util.WorldObjectSettings;
 import processing.core.PImage;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Ore extends Actor
@@ -14,6 +19,30 @@ public class Ore extends Actor
     public Ore(String name, Point position, List<PImage> imgs, int rate)
     {
         super(name, "ore", position, imgs, rate);
+    }
+
+    public Action createTransformAction(World world)
+    {
+        Action a = (long currentTicks) ->
+        {
+            OreBlob blob = ActionManager.createBlob(world,
+                    this.getName() + " -- blob",
+                    this.getPosition(),
+                    (int) (this.getRate() / WorldObjectSettings.BLOB_RATE_SCALE),
+                    currentTicks);
+            this.removeEntity(world);
+            world.addWorldObject(blob);
+            return Collections.singletonList(blob.getPosition());
+        };
+        this.removePendingAction(a);
+        return a;
+    }
+
+    public void schedule(World world, long ticks)
+    {
+        this.scheduleAction(world,
+                this.createTransformAction(world),
+                ticks + this.getRate());
     }
 
     public String entityString()
