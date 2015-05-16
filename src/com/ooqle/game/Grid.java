82de407021a733093c181bff9283ffd011ca5ba6
Kenny Williams <kenny@kennyjwilliams.com>
2015-05-15 17:41:55 -0700

    Made class generic

--- src/com/ooqle/game/Grid.java
+++ src/com/ooqle/game/Grid.java
@@ -1,40 +1,38 @@
 package com.ooqle.game;
 
-import com.ooqle.game.entity.Background;
-import com.ooqle.game.entity.WorldObject;
-
-import java.util.List;
 
 /**
  * Created by augiedoebling on 5/2/15.
  */
-public class Grid {
+public class Grid <T> {
 
     private int width;
     private int height;
-    private WorldObject[][] cells;
+    private T[][] cells;
 
-    public Grid(int width, int height, WorldObject occupancyValue)
+    @SuppressWarnings("unchecked")
+    public Grid(int width, int height, T initValue)
     {
         this.width = width;
         this.height = height;
-        this.cells = new WorldObject[height][width];
+
+        this.cells = (T[][]) new Object[height][width];
 
         for(int r = 0; r < cells.length; r++)
         {
             for(int c = 0; c < cells[r].length; c++)
             {
-                this.cells[r][c] = occupancyValue;
+                this.cells[r][c] = initValue;
             }
         }
     }
 
-    public void setCell(Point pt, WorldObject value)
+    public void setCell(Point pt, T value)
     {
         this.cells[pt.getY()][pt.getX()] = value;
     }
 
-    public WorldObject getCell(Point pt)
+    public T getCell(Point pt)
     {
         return this.cells[pt.getY()][pt.getX()];
     }
