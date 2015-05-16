468fd8bf742746f35195848c9cab15e49de1f2b3
Kenny Williams <kenny@kennyjwilliams.com>
2015-05-15 19:34:51 -0700

    Moved World from util to main package

---
+++ src/com/ooqle/game/World.java
@@ -0,0 +1,161 @@
+package com.ooqle.game;
+/*
+* @author Kenny Williams
+*/
+
+import com.ooqle.game.entity.Background;
+import com.ooqle.game.entity.WorldObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class World
+{
+    private int width;
+    private int height;
+    private ArrayList<WorldObject> worldObjectList;
+    private Grid<Background> backgroundGrid;
+    private Grid<WorldObject> worldObjectGrid;
+
+    public World(int width, int height, Background initBackground)
+    {
+        this.width = width;
+        this.height = height;
+        backgroundGrid = new Grid<>(width, height, initBackground);
+        worldObjectGrid = new Grid<>(width, height, null);
+    }
+
+    public Grid<Background> getBackgroundGrid()
+    {
+        return backgroundGrid;
+    }
+
+    public void setBackgroundGrid(Grid<Background> backgroundGrid)
+    {
+        this.backgroundGrid = backgroundGrid;
+    }
+
+    public Grid<WorldObject> getWorldObjectGrid()
+    {
+        return worldObjectGrid;
+    }
+
+    public void setWorldObjectGrid(Grid<WorldObject> worldObjectGrid)
+    {
+        this.worldObjectGrid = worldObjectGrid;
+    }
+
+    public boolean withinBounds(Point pt)
+    {
+        return pt.getX() >= 0 && pt.getX() < this.width && pt.getY() >= 0 && pt.getY() < this.height;
+    }
+
+    public boolean isOccupied(Point pt)
+    {
+        return this.withinBounds(pt) && this.getWorldObjectGrid().getCell(pt) != null;
+    }
+
+    public void addWorldObject(WorldObject obj)
+    {
+        Point pt = obj.getPosition();
+        if (withinBounds(pt))
+        {
+            WorldObject oldEntity = this.getWorldObjectGrid().getCell(pt);
+            if (oldEntity != null)
+            {
+                //TODO: clearPendingActions is not yet implemented
+                //Need to remove oldEntity from worldObjectList?
+            }
+            this.getWorldObjectGrid().setCell(pt, obj);
+            this.worldObjectList.add(obj);
+        }
+    }
+
+    public void removeEntityAt(Point pt)
+    {
+        if (this.withinBounds(pt) && this.getWorldObjectGrid().getCell(pt) != null)
+        {
+            WorldObject obj = this.worldObjectGrid.getCell(pt);
+            obj.setPosition(new Point(-1, -1));
+            this.worldObjectList.remove(obj);
+            this.getWorldObjectGrid().setCell(pt, null);
+        }
+    }
+
+    public WorldObject getWorldObjectAt(Point pt)
+    {
+        if (this.withinBounds(pt))
+        {
+            return this.getWorldObjectGrid().getCell(pt);
+        }
+        return null;
+    }
+
+    public Background getBackgroundAt(Point pt)
+    {
+        return this.getBackgroundGrid().getCell(pt);
+    }
+
+    public List<Point> moveWorldObject(WorldObject obj, Point pt)
+    {
+        List<Point> tiles = new ArrayList<>(2);
+        if (this.withinBounds(pt))
+        {
+            Point oldPt = obj.getPosition();
+            this.getWorldObjectGrid().setCell(oldPt, null);
+            tiles.add(oldPt);
+            this.getWorldObjectGrid().setCell(pt, obj);
+            tiles.add(pt);
+            obj.setPosition(pt);
+        }
+        return tiles;
+    }
+
+    public List<WorldObject> getWorldObjects()
+    {
+        return this.worldObjectList;
+    }
+
+    public WorldObject findNearestOfType(Point pt, Class type)
+    {
+        HashMap<WorldObject, Integer> oftype = new HashMap<>();
+        for (WorldObject obj : this.worldObjectList)
+        {
+            if (type.isInstance(obj))
+            {
+                oftype.put(obj, pt.distanceSq(obj.getPosition()));
+            }
+        }
+        return nearestEntity(oftype);
+    }
+
+    public static WorldObject nearestEntity(HashMap<WorldObject, Integer> entityDist)
+    {
+        if (!entityDist.isEmpty())
+        {
+            boolean first = true;
+            WorldObject entity = null;
+            Integer integer = null;
+            for (Map.Entry<WorldObject, Integer> other : entityDist.entrySet())
+            {
+                if (first)
+                {
+                    entity = other.getKey();
+                    integer = other.getValue();
+                    first = false;
+                } else
+                {
+                    if (other.getValue() < integer)
+                    {
+                        entity = other.getKey();
+                        integer = other.getValue();
+                    }
+                }
+            }
+            return entity;
+        }
+        return null;
+    }
+}
