aefdd4ded9cef7dc7d489a56959787a82547c8f3
Augie <augustdoebling@gmail.com>
2015-05-16 15:32:43 -0700

    fixed syntax error in saveload

---
+++ txttojson.py
@@ -0,0 +1,90 @@
+import json
+
+try:
+   open("../cpe102hw1/gaia.sav")
+except:
+   print "file not found"
+
+with open("../cpe102hw1/gaia.sav", 'r')as infile:
+   wholefile = infile.readlines()
+
+with open("newworld", 'w+')as outfile:
+   dic = {}
+   wod = []
+   bacd = [] 
+   thing = wholefile[0]
+
+   for x in range(len(wholefile)):
+      theline = wholefile[x]
+      words = theline.split()
+      entity = {}
+
+      if words[0] == "miner":
+         entity['type'] = words[0]
+         
+         loc = {}
+         loc['x'] = words[2]
+         loc['y'] = words[3]
+         entity['location'] = loc
+
+         entity['resourceLimit'] = words[4]
+         entity['rate'] = words[5]
+         entity['animationrate'] = words[6]
+
+         wod.append(entity)
+
+      elif words[0] == "vein":
+         entity['type'] = words[0]
+         
+         loc = {}
+         loc['x'] = words[2]
+         loc['y'] = words[3]
+         entity['location'] = loc
+
+         entity['rate'] = words[4]
+         entity['resourcedistance'] = words[5]
+
+         wod.append(entity)
+
+      elif words[0] == "blacksmith":
+         entity['type'] = words[0]
+         
+         loc = {}
+         loc['x'] = words[2]
+         loc['y'] = words[3]
+         entity['location'] = loc
+
+         entity['resourceLimit'] = words[4]
+         entity['rate'] = words[5]
+         entity['animationrate'] = words[6]
+
+         wod.append(entity)
+
+      elif words[0] == "obstacle":
+         entity['type'] = words[0]
+         
+         loc = {}
+         loc['x'] = words[2]
+         loc['y'] = words[3]
+         entity['location'] = loc
+
+         wod.append(entity)
+
+      elif words[0] == "background":
+         entity['type'] = words[1]
+         
+         loc = {}
+         loc['x'] = words[2]
+         loc['y'] = words[3]
+         entity['location'] = loc
+
+         bacd.append(entity)
+
+      else:
+         print "Error, type: " + words[0] + " undefined"
+
+
+   dic['worldobjects'] = wod
+   dic['background'] = bacd
+
+   outfile.write(json.dumps(dic))
