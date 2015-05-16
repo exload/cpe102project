c28d766661cd389452bfcf1b1159987e1f690ee2
Kenny Williams <kenny@kennyjwilliams.com>
2015-05-15 18:45:44 -0700

    Added getImages and javadocs for getImage and getImages

--- src/com/ooqle/game/Game.java
+++ src/com/ooqle/game/Game.java
@@ -7,7 +7,9 @@ import processing.core.PApplet;
 import processing.core.PImage;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Game extends PApplet
 {
@@ -16,7 +18,7 @@ public class Game extends PApplet
     public HashMap<String, PImage> loadImages(File dir)
     {
         HashMap<String, PImage> out = new HashMap<>();
-        final String[] okFileExtensions = new String[]{"jpg", "png", "gif"};
+        final String[] okFileExtensions = new String[]{"jpg", "png", "gif", "bmp"};
         for (File f : dir.listFiles((File file) -> {
             for (String extension : okFileExtensions)
             {
@@ -61,9 +63,33 @@ public class Game extends PApplet
     Static shiz
      */
 
+    /**
+     * Loads an image given a path. The path root is in the /res directory. To load an image in the directory /res/images/
+     * call example.png call getImage("images/example.png")
+     * @param path The path to the image from the /res directory
+     * @return Returns a path to the image
+     */
     public static PImage getImage(String path)
     {
         return imgs.get(path);
+    }
+
+    /**
+     * Returns a list of PImages following the below format:
+     *      getImages("images/blob*.bmp", 12) will load all images in the images blob1.bmp to blob12.bmp
+     * @param path Path to images. Use one * for the variable.
+     * @param end The final image in the list
+     * @return List of PImages 1 through end
+     */
+    public static List<PImage> getImages(String path, int end)
+    {
+        List<PImage> out = new ArrayList<>();
+        for(int i = 1; i < end + 1; i++)
+        {
+            String newPath = path.replace("*", Integer.toString(i));
+            out.add(getImage(newPath));
+        }
+        return out;
     }
 
     public static void main(String[] args)
