79d231930245b1b0ca19a162417bc2e441b2b488
Kenny Williams <kenny@kennyjwilliams.com>
2015-06-01 00:13:16 -0700

    Added a Toggle Button

---
+++ src/com/ooqle/game/ui/ToggleButton.java
@@ -0,0 +1,38 @@
+package com.ooqle.game.ui;
+/*
+* @author Kenny Williams
+*/
+
+import processing.core.PImage;
+
+public class ToggleButton extends Button
+{
+    private boolean selected;
+    private PImage selectedImg;
+
+    public ToggleButton(int x, int y, PImage image, PImage hoverImg, PImage selectedImg)
+    {
+        super(x, y, image, hoverImg);
+        this.selected = false;
+        this.selectedImg = selectedImg;
+    }
+
+    public boolean isSelected()
+    {
+        return selected;
+    }
+
+    public void setSelected(boolean selected)
+    {
+        this.selected = selected;
+    }
+
+    public PImage getCurrentImage()
+    {
+        if(this.isSelected())
+        {
+            return this.selectedImg;
+        }
+        return super.getCurrentImage();
+    }
+}
