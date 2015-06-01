caca4c6b7581e66cbccf3da85a5717fb817bfaf6
Kenny Williams <kenny@kennyjwilliams.com>
2015-05-31 22:27:25 -0700

    Added getSpriteImages

--- src/com/ooqle/game/util/GameUtils.java
+++ src/com/ooqle/game/util/GameUtils.java
@@ -3,6 +3,11 @@ package com.ooqle.game.util;
 * @author Kenny Williams
 */
 
+import processing.core.PImage;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameUtils
 {
     public static int sign(int x)
@@ -15,5 +20,23 @@ public class GameUtils
             return 1;
         }
         return 0;
+    }
+
+    /**
+     * Loads a sprite into a List of PImages
+     * @param sprite The PImage of the sprite to load into a list
+     * @param numberOfImages The number of images that are in the sprite
+     * @return A list of PImages
+     */
+    public static List<PImage> getSpriteImages(PImage sprite, int numberOfImages)
+    {
+        List<PImage> sprites = new ArrayList<>();
+        int W = sprite.width / numberOfImages;
+        int H = sprite.height;
+        for(int i = 0; i < numberOfImages + 1; i++)
+        {
+            sprites.add(sprite.get(i * W, 0, W, H));
+        }
+        return sprites;
     }
 }
